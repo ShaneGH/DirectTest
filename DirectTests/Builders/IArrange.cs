9a04abd0d0dc9cb0f5fad216179dea7fd78e6194
unknown <shane.connon@gmail.com>
2015-11-22 17:49:54 -0800

    moved classes

--- DirectTests/Builders/IArrange.cs
+++ DirectTests/Builders/IArrange.cs
@@ -23,49 +23,8 @@ namespace DirectTests.Builders
         IArrange BasedOn(string basedOn);
     }
 
-    public interface IFor : ITest
-    {
-        IParameterizedArrange<object> For(MethodInfo method);
-    }
-
-    public interface IFor<TSubject>
-    {
-        IParameterizedArrange<TReturnValue> For<TReturnValue>(Expression<Func<TSubject, TReturnValue>> act);
-    }
-
-    public class TestData : ITestData
-    {
-        public dynamic TestBag { get; private set; }
-        public dynamic Args { get; private set; }
-        public dynamic CArgs { get; private set; }
-
-        public TestData(DynamicBag testBag)
-        {
-            TestBag = testBag;
-            Args = new TestArranger();
-            CArgs = new TestArranger();
-        }
-    }
-
-    public interface ITestData
-    {
-        dynamic TestBag { get; }
-        dynamic Args { get; }
-        dynamic CArgs { get; }
-    }
-
     public interface IParameterizedArrange<TReturnValue> : ITest
     {
         IAssert<TReturnValue> Arrange(Action<ITestData> arrange);
     }
-
-    //public interface IParameterizedAssert : ITest
-    //{
-    //    ITest Assert(Action<ITestData> assert);
-    //}
-
-    //public interface IParameterizedAssert<TActReturnType> : IParameterizedAssert
-    //{
-    //    ITest Assert(Action<ITestData, TActReturnType> assert);
-    //}
 }
